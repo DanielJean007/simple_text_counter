485648069dc7949f9d52c2c74a1f2f9f28400386
DanielJean007 <drodri23@uwo.ca>
2023-01-12 18:08:00 -0300

    removal of unnecessary libs
    removal of unnecessary libs

--- count_words.py
+++ count_words.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, flash, redirect 
 import webbrowser
-import threading
 import string
-import sys
+import os
 
-app = Flask(__name__, template_folder='C:/Users/Daniel Jean/Desktop/voxy/')
+cwd = os.getcwd()
+app = Flask(__name__, template_folder=cwd+'/html_files/')
 app.secret_key = '123456'
 
 @app.route('/')
@@ -18,14 +18,10 @@ def count_words():
         flash('Please enter some text to count words')        
         return redirect('/')
     
-    words = len(text.translate(str.maketrans('','',string.punctuation)).split())
-    
+    words = len(text.translate(str.maketrans('','',string.punctuation)).split())    
     return render_template('count.html', text=text, words=words)
 
 if __name__ == '__main__':    
-    thread = threading.Thread(target=app.run, kwargs={'debug': False, 'use_reloader': False})
-    thread.start()
-    
     webbrowser.open("http://localhost:5000")
-    sys.exit()
+    app.run()    
     
